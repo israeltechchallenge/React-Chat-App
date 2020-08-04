65bdb055cd4805daca974f2ea58ca262dcb4b7d0
yonatan bendahan <yonatan.bendahan@gmail.com>
2020-08-04 10:43:45 +0300

    extract config

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,10 @@ import AppNavbar from "./components/AppNavbar";
 import ChatRoom from "./pages/ChatRoom";
 import UserContext from "./context/UserContext";
 import { User } from "./model/User";
+import firebaseConfig from "./firebaseConfig";
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBF--SiVW26GYBcbfYwTigX5jbycNCcZDM",
-  authDomain: "fullstack-web-course.firebaseapp.com",
-  databaseURL: "https://fullstack-web-course.firebaseio.com",
-  projectId: "fullstack-web-course",
-  storageBucket: "fullstack-web-course.appspot.com",
-  messagingSenderId: "110015518895",
-  appId: "1:110015518895:web:cb25b7d8e9f6efbae69c5a",
-};
 
 firebase.initializeApp(firebaseConfig);
 
